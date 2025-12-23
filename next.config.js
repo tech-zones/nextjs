42493419ff7e70c2e7f3d8d679f4d267f3f9e461
Ayas-HoloFair <126272427+Ayas-HoloFair@users.noreply.github.com>
2025-12-23 23:53:01 +0300

    updates

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
-const path = require('path')
+// const path = require('path')
  
+// module.exports = {
+//   // exportTrailingSlash: true,
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   images: {
+//     domains: ['res.cloudinary.com', 'media.dev.to']
+//   }
+// }
+
+const path = require('path');
+
 module.exports = {
-  // exportTrailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ['res.cloudinary.com', 'media.dev.to']
+    domains: ['res.cloudinary.com', 'media.dev.to', 'media2.dev.to']
   }
-}
+};
